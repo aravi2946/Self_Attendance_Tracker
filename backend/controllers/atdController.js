324d73ecfbf443b44311fe146f717fa4c1974a0b
Aravind <aravi2946@gmail.com>
2025-10-28 08:42:39 +0530

    feat: fetch attendance data

--- backend/controllers/atdController.js
+++ backend/controllers/atdController.js
@@ -2,13 +2,13 @@ import atdModel from "../models/AtdSchema.js";
 
 
 
-
+//adds attendance data 
 const atdController = async (req, res) => {
     const { date, isPresent } = req.body;
     try {
 
-        if (!date || !isPresent) {
-            return res.status(400).json({success:false,msg:"Data not found"})
+        if (!date) {
+            return res.status(400).json({ success: false, msg: "Data not found" })
         }
         //check if user is already exit or not
         const user = await atdModel.findOne({ userId: req.user.id })
@@ -22,11 +22,11 @@ const atdController = async (req, res) => {
                 daily: [{
                     date: date,
                     presents: isPresent ? 1 : 0,
-                    periods:1
+                    periods: 1
                 }]
             })
             await newUser.save();
-           return res.status(200).json({ newUser })
+            return res.status(200).json({ newUser })
         }
 
         const dailyData = user.daily.find(a => a.date.toISOString().slice(0, 10) == date)
@@ -34,17 +34,17 @@ const atdController = async (req, res) => {
             if (dailyData.periods >= 7) {
                 return res.json({ success: false, msg: "You have marked all periods today" })
             }
-         }
+        }
 
         if (!dailyData) {
 
             user.daily.push({
                 date: date,
                 presents: isPresent ? 1 : 0,
-                periods:1
+                periods: 1
             })
 
-        } else if(isPresent) {
+        } else if (isPresent) {
             dailyData.presents += 1;
             dailyData.periods += 1;
 
@@ -55,15 +55,15 @@ const atdController = async (req, res) => {
 
         user.totalPeriods++;
         if (isPresent) {
- 
+
             user.totalPresents++;
         }
-       
+
         user.result = parseFloat((user.totalPresents / user.totalPeriods) * 100).toFixed(2);
 
         await user.save();
 
-        res.status(200).json({success:true,user})
+        res.status(200).json({ success: true, user })
 
 
 
@@ -78,27 +78,93 @@ const atdController = async (req, res) => {
 
 }
 
+//adds previous attendance data
 const addPreviousDataController = async (req, res) => {
-    const {periods,presents} = req.body;
+    const { periods, presents } = req.body;
     try {
         const user = await atdModel.findOne({ userId: req.user.id })
-        
+
         if (!presents || !periods <= 0) {
-            return res.status(400).json({success:false,msg:"Invalid input data"})
+            return res.status(400).json({ success: false, msg: "Invalid input data" })
         }
-            user.totalPeriods = periods;
-            user.totalPresents = presents;
-            user.result = parseFloat((user.totalPresents / user.totalPeriods) * 100).toFixed(2);
-        
+        user.totalPeriods = periods;
+        user.totalPresents = presents;
+        user.result = parseFloat((user.totalPresents / user.totalPeriods) * 100).toFixed(2);
+
 
         await user.save();
-        res.status(200).json({success:true,msg:"Previous data added Successfully"})
-       
+        res.status(200).json({ success: true, msg: "Previous data added Successfully" })
+
     } catch (err) {
         console.log("Error in addPreviousDataController", err);
-        res.status(500).json({msg:"Internal Server Error"})
-        
+        res.status(500).json({ msg: "Internal Server Error" })
+
     }
 }
 
-export { atdController,addPreviousDataController }
+//fetching totalPeriods and totalPresents
+const fetchAtdController = async (req, res) => {
+    try {
+        const user = await atdModel.findOne({ userId: req.user.id })
+
+        if (!user) {
+            return res.status(200).json({success:true, periods: 0, presents: 0, result: 0 })
+        }
+
+        let details = {
+            periods: user.totalPeriods || 0,
+            presents: user.totalPresents || 0,
+            result: user.result||0
+        }
+        return res.status(200).json(details)
+
+
+
+
+    } catch (err) {
+        console.log("Error in fetchAtdController", err);
+        res.status(500).json({ msg: "Internal Server Error" })
+
+    }
+}
+
+//fetching today's periods and presents
+const fetchTodayAtdController = async (req, res) => {
+    const { date } = req.body;
+    try {
+        if (!date) {
+            return res.status(400).json({ success: false, msg: "Pass Today's date " })
+        }
+        const user = await atdModel.findOne({ userId: req.user.id })
+
+        if (!user) {
+            return res.json({ presents: 0, periods: 0 })
+        }
+
+    
+            const daily = await user?.daily?.find(a => a.date.toISOString().slice(0, 10) == date)
+            let dailydata = {
+                presents: daily?.presents||0,
+                periods: daily?.periods||0
+            }
+            res.status(200).json({ success: true, dailydata })
+        
+
+
+
+
+
+
+    } catch (err) {
+        console.log("Error in fetchAtdController", err);
+        res.status(500).json({ msg: "Internal Server Error" })
+
+    }
+}
+
+
+
+
+
+
+export { atdController, addPreviousDataController, fetchAtdController, fetchTodayAtdController }
--- backend/routes/atdRoutes.js
+++ backend/routes/atdRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from "express"
-import { addPreviousDataController, atdController } from "../controllers/atdController.js";
+import { addPreviousDataController, atdController, fetchAtdController, fetchTodayAtdController } from "../controllers/atdController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
 
 const atdRoutes = Router();
 
 atdRoutes.post('/add',authMiddleware, atdController)
-atdRoutes.post('/add-prev',authMiddleware,addPreviousDataController)
+atdRoutes.post('/add-prev', authMiddleware, addPreviousDataController)
+atdRoutes.get('/', authMiddleware, fetchAtdController)
+atdRoutes.post('/today',authMiddleware,fetchTodayAtdController)
+
 export default atdRoutes;
