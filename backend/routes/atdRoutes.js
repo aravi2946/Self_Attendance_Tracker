91a912addee74e46caf718dd8dc4eca38923457d
Aravind <aravi2946@gmail.com>
2025-11-08 20:07:05 +0530

    update atdRoutes

--- backend/routes/atdRoutes.js
+++ backend/routes/atdRoutes.js
@@ -5,10 +5,10 @@ import authMiddleware from "../middleware/authMiddleware.js";
 
 const atdRoutes = Router();
 
-atdRoutes.post('/add',authMiddleware, atdController)
+atdRoutes.post('/add', authMiddleware, atdController)
 atdRoutes.post('/add-prev', authMiddleware, addPreviousDataController)
 atdRoutes.get('/', authMiddleware, fetchAtdController)
 atdRoutes.post('/today', authMiddleware, fetchTodayAtdController)
-atdRoutes.put('/reset',authMiddleware,resetAtdDataController)
+atdRoutes.put('/reset', authMiddleware, resetAtdDataController)
 
 export default atdRoutes;
