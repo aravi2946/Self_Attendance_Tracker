99642c179cd5dae36e297e46366405c196f932e7
Aravind <aravi2946@gmail.com>
2025-12-16 20:59:16 +0530

    update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: "0.0.0.0",
+    host: "localhost",
     port: 5173,
   },
   resolve: {
