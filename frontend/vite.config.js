ad1afd80067e64c8f199667b5c01b55f3247f0d4
Aravind <aravi2946@gmail.com>
2025-12-16 19:41:59 +0530

    update ui styling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,34 +1,27 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     host: '0.0.0.0',
-//     port:5173,
-//   }
-// })
-
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [['babel-plugin-react-compiler', { target: '19' }]],
-    },
-  }), tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: '0.0.0.0',
-    port:5173
+    host: "0.0.0.0",
+    port: 5173,
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+        },
+      },
     },
   },
 })
